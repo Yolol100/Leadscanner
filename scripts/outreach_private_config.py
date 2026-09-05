30947827464ef4daad44fd14a2a6765244ee468e
Yolol100 <Andrew_Baeten@hotmail.com>
2026-09-05 23:33:04 +0200

    Move outreach postal config to private Drive folder

--- scripts/outreach_private_config.py
+++ scripts/outreach_private_config.py
@@ -6,7 +6,7 @@ import sys
 
 from outreach_sender import build_sheets_service, get_values
 
-DEFAULT_CONFIG_SPREADSHEET_ID = "1panRChrQAL-Fe067swRGUCP3mxdyXA6hmvUUwdmQxfI"
+DEFAULT_CONFIG_SPREADSHEET_ID = "1iOBCaBq3MpsrYQiESAhjS1NMRQDXAWmkTh-U-63qzs4"
 CONFIG_SHEET = "Config"
 POSTAL_KEY = "OUTREACH_POSTAL_ADDRESS"
 
